b06e4225196cb70b896dc9dc024175dd79eb836a
alex-baiet <alex.baiet3@gmail.com>
2021-11-03 12:48:35 +0100

    Structuration du projet

---
+++ class/pdo.inc.php
@@ -0,0 +1,14 @@
+<?php if (!defined("DEF_PDO")) {
+define("DEF_PDO", true);
+
+// Créer une connexion à la base de donnée
+// INFORMATIONS NON TESTEES
+$user =  "guillaume.grisolet";
+$pass =  "guillaumé el famoso";
+try {
+  $pdo = new PDO('pgsql:host=sqletud.u-pem.fr;dbname=guillaume.grisolet_db',$user,$pass);
+} catch (PDOException $e) {
+  echo "ERREUR : La connexion a échouée.";
+}
+
+}?>
