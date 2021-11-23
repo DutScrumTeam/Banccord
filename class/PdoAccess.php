fcec908305db7769e67c3751b83a874b768bbc5a
Enderclem0 <cduval07@etud.u-pem.fr>
2021-11-23 08:21:00 +0100

    Supression new account, new client

--- class/PdoAccess.php
+++ class/PdoAccess.php
@@ -2,58 +2,89 @@
 
 class PdoAccess
 {
-private static $user =  "clement.duval";
-private static $pass =  "6A3iMDg5UZeR8jz";
-private static $database = "guillaume.grisolet_db";
-private static $host = "sqletud.u-pem.fr";
-private static  $pdo;
-public static function getPdo(): PDO
-{
-	if (self::$pdo === null) {
-		try {
-			echo "Connexion à la base de données en cours...\n";
-			self::$pdo = new PDO('pgsql:host='.PdoAccess::$host.';dbname='.PdoAccess::$database, self::$user, self::$pass);
-		} catch (PDOException $e) {
-			echo "ERREUR : La connexion a échouée<br>\n";
-			echo $e->getMessage()."<br>\n";
-		}
-	}
-	return self::$pdo;
-}
-public static function insertAccount($name, $password, $type){
-	$pdo = self::getPdo();
-	$password = md5($password);
-    $sql = "INSERT INTO banque.compte VALUES (:name,:password,:type)";
-    $stmt = $pdo->prepare($sql);
-    $stmt->bindParam(':name', $name);
-    $stmt->bindParam(':password', $password);
-    $stmt->bindParam(':type', $type);
-    $stmt->execute();
-}
-public static function insertClient($name,$password,$siren,$businessName){
-	self::insertAccount($name,$password,"Client");
-	$pdo = self::getPdo();
-	$sql = "INSERT INTO banque.client VALUES (:num_siren,:raison_social,:id_compte)";
-	$stmt = $pdo->prepare($sql);
-	$stmt->bindParam(':num_siren', $siren);
-	$stmt->bindParam(':raison_social', $businessName);
-	$stmt->bindParam(':id_compte', $name);
-	$stmt->execute();
-}
+	private static $user = "clement.duval";
+	private static $pass = "6A3iMDg5UZeR8jz";
+	private static $database = "guillaume.grisolet_db";
+	private static $host = "sqletud.u-pem.fr";
+	private static $pdo;
 
-	public static function checkUser($pseudo,$password)
+	public static function getPdo(): PDO
+	{
+		if (self::$pdo === null) {
+			try {
+				self::$pdo = new PDO('pgsql:host=' . PdoAccess::$host . ';dbname=' . PdoAccess::$database, self::$user, self::$pass);
+			} catch (PDOException $e) {
+				echo "ERREUR : La connexion a échouée<br>\n";
+				echo $e->getMessage() . "<br>\n";
+			}
+		}
+		return self::$pdo;
+	}
+
+	public static function insertAccount($name, $password, $type)
 	{
 		$pdo = self::getPdo();
-        $sql = "SELECT * FROM banque.compte WHERE id = :pseudo";
-        $stmt = $pdo->prepare($sql);
+		$password = md5($password);
+		$sql = "INSERT INTO banque.compte VALUES (:name,:password,:type)";
+		$stmt = $pdo->prepare($sql);
+		$stmt->bindParam(':name', $name);
+		$stmt->bindParam(':password', $password);
+		$stmt->bindParam(':type', $type);
+		$stmt->execute();
+	}
+
+	public static function insertClient($name, $password, $siren, $businessName)
+	{
+		self::insertAccount($name, $password, "Client");
+		$pdo = self::getPdo();
+		$sql = "INSERT INTO banque.client VALUES (:num_siren,:raison_social,:id_compte)";
+		$stmt = $pdo->prepare($sql);
+		$stmt->bindParam(':num_siren', $siren);
+		$stmt->bindParam(':raison_social', $businessName);
+		$stmt->bindParam(':id_compte', $name);
+		$stmt->execute();
+	}
+
+	public static function deleteAccount($name)
+	{
+		$pdo = self::getPdo();
+		$sql = "DELETE FROM banque.compte WHERE id = :name";
+		$stmt = $pdo->prepare($sql);
+		$stmt->bindParam(':name', $name);
+		$stmt->execute();
+	}
+
+	public static function checkUser($pseudo, $password)
+	{
+		$pdo = self::getPdo();
+		$sql = "SELECT * FROM banque.compte WHERE id = :pseudo";
+		$stmt = $pdo->prepare($sql);
 		$stmt->bindParam(':pseudo', $pseudo);
-        $stmt->execute();
-        $result = $stmt->fetch();
-        if ($result) {
-            if ($result['mdp'] == md5($password)) {
-                return $result['type_compte'];
-            }
-        }
-        return null;
+		$stmt->execute();
+		$result = $stmt->fetch();
+		if ($result) {
+			if ($result['mdp'] == md5($password)) {
+				return $result['type_compte'];
+			}
+		}
+		return null;
+	}
+
+	public static function adminAccountTable()
+	{
+		$pdo = self::getPdo();
+		$sql = "SELECT id,num_siren FROM banque.compte JOIN  banque.client ON compte.id = client.id_compte";
+		$stmt = $pdo->prepare($sql);
+		$stmt->execute();
+		$result = $stmt->fetchAll();
+		foreach ($result as $row) {
+			echo "<tr>";
+			echo "<form action='admin.php?id=" . $row['id'] . " method='get'>";
+			echo "<td>" . $row['id'] . "</td>";
+			echo "<td>" . $row['num_siren'] . "</td>";
+			echo "<td><input type='submit' class='btn btn-danger' value='Supprimer' name='delete'></td>";
+			echo "</form>";
+			echo "</tr>";
+		}
 	}
 }
